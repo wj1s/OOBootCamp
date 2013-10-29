77e0236915c4ba4367983bca3c2f847985bf3d3e
Wang Jian <jianwang@thoughtworks.com>
2013-10-29 18:03:58 +0800

    init

---
+++ OOBootCamp/Length.cs
@@ -0,0 +1,18 @@
+﻿namespace OOBootCamp
+{
+    public class Length
+    {
+        public int Value { get; set; }
+
+        public Length(int value, Unit unit)
+        {
+            Value = value;
+        }
+
+        public override bool Equals(object obj)
+        {
+            var length = (Length)obj;
+            return Value == length.Value;
+        }
+    }
+}
---
+++ OOBootCamp/LengthFact.cs
@@ -0,0 +1,22 @@
+﻿using Xunit;
+
+namespace OOBootCamp
+{
+    public class LengthFact
+    {
+        [Fact]
+        public void should_length_equal()
+        {
+            Assert.Equal(new Length(1, Unit.M), new Length(1, Unit.M));
+            Assert.Equal(new Length(100, Unit.CM), new Length(100, Unit.CM));
+            Assert.Equal(new Length(10, Unit.MM), new Length(10, Unit.MM));
+        }
+    }
+
+    public enum Unit
+    {
+        CM,
+        M,
+        MM
+    }
+}
